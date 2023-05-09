38df08a596f60423026aafcb7dc7cc74d4e9e043
jdbenergy <104963515+jon-jackfruit@users.noreply.github.com>
2023-05-09 20:25:04 +0530

    changed to heroku setup

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__)
+from dash import Dash, dcc, html, Input, Output
+import os
 
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+app.layout = html.Div([
+    html.H1('Hello World'),
+    dcc.Dropdown(['LA', 'NYC', 'MTL'],
+        'LA',
+        id='dropdown'
+    ),
+    html.Div(id='display-value')
+])
+
+@app.callback(Output('display-value', 'children'),
+                [Input('dropdown', 'value')])
+def display_value(value):
+    return f'You have selected {value}'
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+    
