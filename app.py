225510969af818590d3e3fc6969479b5805d7331
jdbenergy <104963515+jon-jackfruit@users.noreply.github.com>
2023-05-16 17:34:12 +0530

    added static file server WhiteNoise

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
+from whitenoise import WhiteNoise
 
-df = pd.read_csv('combined_data.csv')
+# Setup app and server
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
-app = Dash(__name__)
+# add a static file server
+server.wsgi_app=WhiteNoise(server.wsgi_app, root='static/')
 
+# load data
+df = pd.read_csv('static/combined_data.csv')
+
+# build app
 app.layout = html.Div([
     html.H1(children='State of Indian utilities', style={'textAlign':'center'}),
     dcc.Dropdown(df.State.unique(), 'Assam', id='dropdown-selection'),
@@ -20,6 +29,7 @@ def update_graph(value):
     dff = df[df.State==value]
     return px.line(dff, x='Year', y='Cost-Gross Input Energy (MU)')
 
+# start server
 if __name__ == '__main__':
     app.run_server(debug=True)
 
